431a314d2d801a22a212d9cf657daf0f7a047e19
Oumayma Mtat <oumaymamtat20@gmail.com>
2021-07-30 08:38:53 +0100

    First commit

---
+++ Angular/src/app/shared.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  readonly APIUrl ="http://localhost:5000/api";
+  readonly PhotoUrl = "http://localhost:5000/Photos";
+
+  constructor(private http:HttpClient) { }
+
+  // Methods to Consume Department Data API
+
+  getDepList():Observable<any[]>{
+    return this.http.get<any>(this.APIUrl+'/department');
+  }
+
+  addDepartment(val:any){
+    return this.http.post(this.APIUrl+'/department',val);
+  }
+
+  updateDepartment(val:any){
+    return this.http.put(this.APIUrl+'/department',val);
+  }
+
+  deleteDepartment(val:any){
+    return this.http.delete(this.APIUrl+'/department/'+val);
+  }
+
+  // Methods to Consume Employee Data API
+
+  getEmpList():Observable<any[]>{
+    return this.http.get<any>(this.APIUrl+'/Employee');
+  }
+
+  addEmployee(val:any){
+    return this.http.post(this.APIUrl+'/Employee',val);
+  }
+
+  updateEmployee(val:any){
+    return this.http.put(this.APIUrl+'/Employee',val);
+  }
+
+  deleteEmployee(val:any){
+    return this.http.delete(this.APIUrl+'/Employee',val)
+  }
+
+  // Method to save profile Picture
+
+  UploadPhoto(val:any){
+    this.http.post(this.APIUrl+'/Employee/SaveFile',val);
+  }
+
+  // Method to get All Department Names
+  getAllDepartmentNames():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl+'/Employee/GetAllDepartmentNames');
+  }
+
+
+
+
+}
